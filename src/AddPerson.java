35ed38d66f273e828cb4375b93ad00f8e81ebf4d
thepaperpilot <thepaperpilot@gmail.com>
2014-12-22 17:34:59 -0600

    Started process of making an add person button

---
+++ src/AddPerson.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class AddPerson {
+private JPanel cards;
+private JPanel settings;
+private JTextField personName;
+private JButton create;
+private JSpinner numGroup;
+private JButton cancel;
+private JLabel add;
+
+public AddPerson() {
+	create.addActionListener(new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent actionEvent) {
+			// TODO How to deal with different group types having different structures? (as in, I need to send a value for family here, and people's families are currently uneditable)
+			// ChristmasExchange.getGroup().addPerson()
+		}
+	});
+	add.addMouseListener(new MouseAdapter() {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			((CardLayout) cards.getLayout()).show(cards, "settings");
+		}
+	});
+	cancel.addActionListener(new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent actionEvent) {
+			((CardLayout) cards.getLayout()).show(cards, "add");
+		}
+	});
+}
+
+private void createUIComponents() {
+	settings = new Shadow();
+	personName = new JTextField();
+	personName.setBorder(BorderFactory.createEmptyBorder());
+}
+}
