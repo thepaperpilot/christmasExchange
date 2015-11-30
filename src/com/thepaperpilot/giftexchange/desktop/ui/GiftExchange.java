c3807f054bb4ad1a8fdc21c8493351d4c64227b8
thepaperpilot <thepaperpilot@gmail.com>
2015-11-30 00:05:16 -0600

    Made it use system themes

--- src/com/thepaperpilot/giftexchange/desktop/ui/GiftExchange.java
+++ src/com/thepaperpilot/giftexchange/desktop/ui/GiftExchange.java
@@ -23,17 +23,6 @@ private JPanel panel;
 private JTabbedPane tabs;
 
 public static void main(String[] args) {
-	try {
-		for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-			if ("Nimbus".equals(info.getName())) {
-				UIManager.setLookAndFeel(info.getClassName());
-				break;
-			}
-		}
-	} catch (Exception e) {
-		// If Nimbus is not available, you can set the GUI to another look and feel.
-	}
-
 	JFrame frame = new JFrame("Christmas Exchange");
 	frame.setContentPane(instance.panel);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
